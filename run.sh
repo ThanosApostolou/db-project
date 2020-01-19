4ed74f554c4ffa617ce1be0e84afaab3680f64cd
ThanosApostolou <thanosapostolou@outlook.com>
2020-01-19 15:22:04 +0200

    add Q1-MR output

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#hdfs dfs -get /Q1-MR-out ./out/
-#hdfs dfs -text /Q1-MR-out/* > ./out/Q1-MR-out.txt
+#hdfs dfs -get /Q1-MR-out ./output
+#hdfs dfs -text /Q1-MR-out/* > ./output/Q1-MR-out.txt
 
 if [ "$1" = "1a" ]; then
     hdfs dfs -rm -r /Q1-MR-out
