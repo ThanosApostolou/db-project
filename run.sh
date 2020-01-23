5e2415e1e5801abc61d2aec7e8dd8ed0b1a2e07e
ThanosApostolou <thanosapostolou@outlook.com>
2020-01-23 21:44:52 +0200

    add new logs for Q1

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ if [ "$1" = "1a" ]; then
 elif [ "$1" = "1b" ]; then
     time spark-submit ./src/Q1-SQL.py 2>&1 | tee ./logs/Q1-SQL/master.log
 elif [ "$1" = "1c" ]; then
-    time spark-submit ./src/Q1-SQL-PARQUET.py 2>&1 | tee ./logs/Q1-PARQUET-SQL/master.log
+    time spark-submit ./src/Q1-SQL-PARQUET.py 2>&1 | tee ./logs/Q1-SQL-PARQUET/master.log
 elif [ "$1" = "2a" ]; then
     hdfs dfs -rm -r /Q2-MR-out
     time spark-submit ./src/Q2-MR.py 2>&1 | tee ./logs/Q2-MR/master.log
